d93250835910db621f0d5533572d2f6bb357562e
Pedro Serpa <pedroserpah@gmail.com>
2021-04-01 22:15:33 -0300

    gerando casos de testes ordenados

--- resultados.py
+++ resultados.py
@@ -29,15 +29,15 @@ for k in range(10):
         os.system(f'./Btree/btree < ./rand_ins{k}/in{i} > out_b')
     
 
-# out_avltree = open('outs_piorcaso/out_avltree', 'w').close()
-# out_btree = open('outs_piorcaso/out_btree', 'w').close()
+out_avltree = open('outs_piorcaso/out_avltree', 'w').close()
+out_btree = open('outs_piorcaso/out_btree', 'w').close()
 
-# for i in range(n):
-#     os.system(f' ./avltree/avl < ./ins/in{i} > out_avl')
-#     os.system(f'./Btree/btree < ./ins/in{i} > out_b')
+for i in range(n):
+    os.system(f' ./avltree/avl < ./ins/in{i} > out_avl')
+    os.system(f'./Btree/btree < ./ins/in{i} > out_b')
 
-#     out_avltree = open('./outs_piorcaso/out_avltree', 'a')
-#     out_btree = open('./outs_piorcaso/out_btree', 'a')
-#     out_avltree.close()
-#     out_btree.close()
+    out_avltree = open('./outs_piorcaso/out_avltree', 'a')
+    out_btree = open('./outs_piorcaso/out_btree', 'a')
+    out_avltree.close()
+    out_btree.close()
 
